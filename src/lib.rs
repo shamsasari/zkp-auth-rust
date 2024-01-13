baf6273abcc5ccd6a467fb56bc1fbb904d3bb624
Shams Asari <shams.asari@gmail.com>
2024-01-13 22:16:40 +0000

    Extracted public params into struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,36 @@
-use num_bigint::BigUint;
+use num_bigint::{BigUint, RandBigInt};
+use rand::thread_rng;
 
-pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint) -> BigUint {
-    // TODO Do we need modpow?
-    return if *k >= c * x {
-        (k - c * x) % q
-    } else {
-        q - (c * x - k) % q
+pub struct ChaumPedersen {
+    p: BigUint,
+    q: BigUint,
+    alpha: BigUint,
+    beta: BigUint
+}
+
+impl ChaumPedersen {
+    pub fn generate_pair(&self, exponent: &BigUint) -> (BigUint, BigUint) {
+        return (self.alpha.modpow(exponent, &self.p), self.beta.modpow(exponent, &self.p));
+    }
+
+    pub fn solve(&self, k: &BigUint, c: &BigUint, x: &BigUint) -> BigUint {
+        return if *k >= c * x {
+            (k - c * x) % &self.q
+        } else {
+            &self.q - (c * x - k) % &self.q
+        }
+    }
+
+    pub fn verify(&self, r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, c: &BigUint, s: &BigUint) -> bool {
+        let (alpha_power_s, beta_power_s) = self.generate_pair(s);
+        let calculated_r1 = (alpha_power_s * y1.modpow(c, &self.p)) % &self.p;
+        let calculated_r2 = (beta_power_s * y2.modpow(c, &self.p)) % &self.p;
+        return *r1 == calculated_r1 && *r2 == calculated_r2
     }
 }
 
-pub fn verify(
-    r1: &BigUint, r2: &BigUint,
-    y1: &BigUint, y2: &BigUint,
-    alpha: &BigUint,
-    beta: &BigUint,
-    c: &BigUint,
-    s: &BigUint,
-    p: &BigUint
-) -> bool {
-    let calculated_r1 = (alpha.modpow(s, p) * y1.modpow(c, p)) % p;
-    let calculate_r2 = (beta.modpow(s, p) * y2.modpow(c, p)) % p;
-    return *r1 == calculated_r1 && *r2 == calculate_r2
+pub fn generate_random(limit: &BigUint) -> BigUint {
+    return thread_rng().gen_biguint_below(limit)
 }
 
 #[cfg(test)]
@@ -28,49 +38,68 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basic_success() {
-        let alpha = BigUint::from(4u32);
-        let beta = BigUint::from(9u32);
-        let p = BigUint::from(23u32);
-        let q = BigUint::from(11u32);
+    fn success_with_hardcoded_values_for_k_and_c() {
+        let cp = ChaumPedersen {
+            p: BigUint::from(23u32),
+            q: BigUint::from(11u32),
+            alpha: BigUint::from(4u32),
+            beta: BigUint::from(9u32)
+        };
+
         let x = BigUint::from(6u32);
         let k = BigUint::from(7u32);
         let c = BigUint::from(4u32);
 
-        let y1 = alpha.modpow(&x, &p);
-        let y2 = beta.modpow(&x, &p);
+        let (y1, y2) = cp.generate_pair(&x);
+        let (r1, r2) = cp.generate_pair(&k);
 
-        let r1 = alpha.modpow(&k, &p);
-        let r2 = beta.modpow(&k, &p);
-
-        let s = solve(&k, &c, &x, &q);
+        let s = cp.solve(&k, &c, &x);
         assert_eq!(s, BigUint::from(5u32));
 
-        let is_verified = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        let is_verified = cp.verify(&r1, &r2, &y1, &y2, &c, &s);
         assert!(is_verified);
     }
 
     #[test]
-    fn basic_failure() {
-        let alpha = BigUint::from(4u32);
-        let beta = BigUint::from(9u32);
-        let p = BigUint::from(23u32);
-        let q = BigUint::from(11u32);
+    fn failure_with_attempt_to_mitm() {
+        let cp = ChaumPedersen {
+            p: BigUint::from(23u32),
+            q: BigUint::from(11u32),
+            alpha: BigUint::from(4u32),
+            beta: BigUint::from(9u32)
+        };
         let x = BigUint::from(6u32);
         let k = BigUint::from(7u32);
         let c = BigUint::from(4u32);
 
-        let y1 = alpha.modpow(&x, &p);
-        let y2 = beta.modpow(&x, &p);
-
-        let r1 = alpha.modpow(&k, &p);
-        let r2 = beta.modpow(&k, &p);
+        let (y1, y2) = cp.generate_pair(&x);
+        let (r1, r2) = cp.generate_pair(&k);
 
         // Man in the middle tries to intercept with a fake secret
         let fake_x = BigUint::from(9u32);
-        let fake_s = solve(&k, &c, &fake_x, &q);
+        let fake_s = cp.solve(&k, &c, &fake_x);
 
-        let is_verified = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &fake_s, &p);
+        let is_verified = cp.verify(&r1, &r2, &y1, &y2, &c, &fake_s);
         assert!(!is_verified);
+    }
+
+    #[test]
+    fn success_with_random_values_for_k_and_c() {
+        let cp = ChaumPedersen {
+            p: BigUint::from(23u32),
+            q: BigUint::from(11u32),
+            alpha: BigUint::from(4u32),
+            beta: BigUint::from(9u32)
+        };
+        let x = BigUint::from(6u32);
+        let k = generate_random(&cp.q);
+        let c = generate_random(&cp.q);
+
+        let (y1, y2) = cp.generate_pair(&x);
+        let (r1, r2) = cp.generate_pair(&k);
+
+        let s = cp.solve(&k, &c, &x);
+        let is_verified = cp.verify(&r1, &r2, &y1, &y2, &c, &s);
+        assert!(is_verified);
     }
 }
