c7c249958713beb09356eeee69879ade782f16ad
Shams Asari <shams.asari@gmail.com>
2024-01-13 23:03:32 +0000

    Testing with real large prime-order group

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,7 @@ pub fn generate_random(limit: &BigUint) -> BigUint {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num::{Num};
 
     #[test]
     fn success_with_hardcoded_values_for_k_and_c() {
@@ -92,6 +93,43 @@ mod tests {
             beta: BigUint::from(9u32)
         };
         let x = BigUint::from(6u32);
+        let k = generate_random(&cp.q);
+        let c = generate_random(&cp.q);
+
+        let (y1, y2) = cp.generate_pair(&x);
+        let (r1, r2) = cp.generate_pair(&k);
+
+        let s = cp.solve(&k, &c, &x);
+        let is_verified = cp.verify(&r1, &r2, &y1, &y2, &c, &s);
+        assert!(is_verified);
+    }
+
+    #[test]
+    fn success_on_actual_prime_order_group() {
+        // Values taken from https://datatracker.ietf.org/doc/html/rfc5114#section-2.1
+        let p = BigUint::from_str_radix(
+            "B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B61\
+                6073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BF\
+                ACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0\
+                A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371",
+            16
+        ).unwrap();
+        let g = BigUint::from_str_radix(
+            "A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31\
+                266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4\
+                D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28A\
+                D662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5",
+            16
+        ).unwrap();
+        let q = BigUint::from_str_radix("F518AA8781A8DF278ABA4E7D64B7CB9D49462353", 16).unwrap();
+        let cp = ChaumPedersen {
+            p: p.clone(),
+            q: q.clone(),
+            alpha: g.clone(),
+            // alpha^i is also a generator
+            beta: g.modpow(&generate_random(&q), &p)
+        };
+        let x = generate_random(&cp.q);
         let k = generate_random(&cp.q);
         let c = generate_random(&cp.q);
 
